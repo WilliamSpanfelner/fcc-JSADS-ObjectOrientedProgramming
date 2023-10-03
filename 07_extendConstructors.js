e86618d4e479fbab9f1248a6a25a68ce49659e56
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 15:40:59 +0300

    Amend constructor to accept arguments

--- 07_extendConstructors.js
+++ 07_extendConstructors.js
@@ -1,1 +1,14 @@
 // Extend Constructors to Receive Arguments
+
+/* The Bird constructor can be modified to assign values
+to properties at the time of creation */
+
+function Bird(name, color) {
+    this.name = name;
+    this.color  = color;
+    this.numLegs = 2;
+}
+
+// Now pass in arguments to create a new bird.
+let cardinal = new Bird("Bruce", "red");
+console.log(cardinal.name, cardinal.color, cardinal.numLegs);

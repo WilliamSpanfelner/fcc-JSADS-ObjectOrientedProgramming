5b97e7f9fbe9e37a99b0c75f267a4f19e5a181f0
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 17:25:08 +0300

    Illustrate own properties

--- 09_ownProperties.js
+++ 09_ownProperties.js
@@ -1,1 +1,12 @@
 // Understand Own Properties
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+
+let duck = new Bird("Donald");
+let canary = new Bird("Tweety");
+
+/* The properties name and numLegs are called own properties
+since the instance object defines them. */
+

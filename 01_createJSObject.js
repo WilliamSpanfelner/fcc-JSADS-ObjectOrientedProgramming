d41c04c1265c48ab20f9e77fbb901c7e85234b10
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 14:24:03 +0300

    Challenge - start

--- 01_createJSObject.js
+++ 01_createJSObject.js
@@ -4,3 +4,9 @@ let duck = {
     name: "Aflac",
     numLegs: 2
 };
+
+/*Create a dog object with name and numLegs properties, and set them 
+to a string and a number, respectively.*/
+let dog = {
+
+};

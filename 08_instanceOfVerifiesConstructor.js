b24f29588bb87a07dc172a7ab39c01331c9db92a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 17:14:54 +0300

    Challenge - start

--- 08_instanceOfVerifiesConstructor.js
+++ 08_instanceOfVerifiesConstructor.js
@@ -20,3 +20,12 @@ let canary = {
 }
 
 console.log(canary instanceof Bird);  // false
+
+/* Create a new instance of the House constructor, calling 
+it myHouse and passing a number of bedrooms. Then, use 
+instanceof to verify that it is an instance of House. */
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+}
+  
+// Only change code below this line

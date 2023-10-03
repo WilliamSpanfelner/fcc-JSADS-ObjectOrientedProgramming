94c7d1f78a86e5563498bdd81b16350215258e91
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 14:48:45 +0300

    Challenge - solution

--- 03_createMethodOnObject.js
+++ 03_createMethodOnObject.js
@@ -14,7 +14,7 @@ should return the sentence This dog has 4 legs. */
 let dog = {
     name: "Spot",
     numLegs: 4,
-  
+    sayLegs: function() {return "This dog has " + dog.numLegs + " legs.";}
   };
   
-  dog.sayLegs();
+  console.log(dog.sayLegs());

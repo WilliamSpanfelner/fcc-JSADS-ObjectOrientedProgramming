dfae6ae1020b2ff8b0b9bc7f125f96c19423e66e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 18:35:54 +0300

    Add prototype property to constructor

--- 10_prototypeProperties.js
+++ 10_prototypeProperties.js
@@ -1,1 +1,14 @@
 // Use Prototype Properties to Reduce Duplicate Code
+function Bird(name, color) {
+    this.name = name;
+    this.color = color;
+}
+
+Bird.prototype.numLegs = 2;
+
+let duck = new Bird("Donald", "blue");
+let canary = new Bird("Tweety", "yellow");
+console.log(duck.numLegs);  // 2
+console.log(canary.numLegs);  // 2
+
+// Using prototype properties reduces code duplication

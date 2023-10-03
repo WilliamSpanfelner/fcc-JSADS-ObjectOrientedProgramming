77d652c981b7bc5e3ce5f772bd3e0d4a8dab3863
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 18:51:57 +0300

    Create array with prototype properties

--- 11_iterateProperties.js
+++ 11_iterateProperties.js
@@ -1,2 +1,28 @@
 // Iterate Over All Properties
 
+/* Own properties are defined directly on the object 
+instance itself. And prototype properties are defined 
+on the prototype. */
+
+function Bird(name) {
+    this.name = name;
+}
+
+Bird.prototype.numLegs = 2;
+
+let duck = new Bird("Donald");
+
+// Now create ownProps and prototypeProps arrays:
+let ownProps = [];
+let prototypeProps = [];
+
+for (const property in duck) {
+    if (duck.hasOwnProperty(property)) {
+        ownProps.push(property);
+    } else {
+        prototypeProps.push(property);
+    }
+}
+
+console.log(ownProps);
+console.log(prototypeProps);

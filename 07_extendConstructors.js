b9d7f0e624c13152652ba3d6d11bc20c64d78150
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 15:42:45 +0300

    Challenge - start

--- 07_extendConstructors.js
+++ 07_extendConstructors.js
@@ -12,3 +12,12 @@ function Bird(name, color) {
 // Now pass in arguments to create a new bird.
 let cardinal = new Bird("Bruce", "red");
 console.log(cardinal.name, cardinal.color, cardinal.numLegs);
+
+/* Create another Dog constructor. This time, set it up to take the 
+parameters name and color, and have the property numLegs fixed at 
+4. Then create a new Dog saved in a variable terrier. Pass it two 
+strings as arguments for the name and color properties. */
+
+function Dog() {
+    
+}

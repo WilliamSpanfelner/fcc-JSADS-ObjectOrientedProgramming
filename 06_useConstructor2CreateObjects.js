3a4ec0d5be8f4a99cc83f60338c6f7999f368c96
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 15:22:08 +0300

    Create a new object instance

--- 06_useConstructor2CreateObjects.js
+++ 06_useConstructor2CreateObjects.js
@@ -1,1 +1,11 @@
 // Use a Constructor to Create Objects
+
+function Bird() {
+  this.name = "Albert";
+  this.color  = "blue";
+  this.numLegs = 2;
+}
+
+// Use the new keyword to create a new object instance
+
+let blueBird = new Bird();

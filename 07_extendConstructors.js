e7c31677618820a70fd8562d9a8561d91f5621b7
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 15:47:10 +0300

    Challenge - solution

--- 07_extendConstructors.js
+++ 07_extendConstructors.js
@@ -18,6 +18,11 @@ parameters name and color, and have the property numLegs fixed at
 4. Then create a new Dog saved in a variable terrier. Pass it two 
 strings as arguments for the name and color properties. */
 
-function Dog() {
-    
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
 }
+
+let terrier = new Dog("Tez", "white");
+console.log(terrier.name, terrier.color, terrier.numLegs);

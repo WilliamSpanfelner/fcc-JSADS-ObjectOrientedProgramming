b63269b6987aec3423d1e28ea067bb1ed8cec94e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 17:30:13 +0300

    Create array of properties using for in loop & hasOwnProperty function

--- 09_ownProperties.js
+++ 09_ownProperties.js
@@ -10,3 +10,12 @@ let canary = new Bird("Tweety");
 /* The properties name and numLegs are called own properties
 since the instance object defines them. */
 
+let ownProps = [];
+
+for (const property in duck) {
+    if (duck.hasOwnProperty(property)) {
+        ownProps.push(property);
+    }
+}
+
+console.log(ownProps);  // ['name', 'numLegs']

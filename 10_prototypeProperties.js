70a1c903328ef047ef562a59846d6105ab36fbee
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 18:39:55 +0300

    Add prototype property to constructor

--- 10_prototypeProperties.js
+++ 10_prototypeProperties.js
@@ -16,9 +16,11 @@ console.log(canary.numLegs);  // 2
 // Add a numLegs property to the prototype of Dog
 function Dog(name) {
     this.name = name;
-  }
-  
-  
-  
-  // Only change code above this line
-  let beagle = new Dog("Snoopy");
+}
+
+Dog.prototype.numLegs = 4;
+
+// Only change code above this line
+let beagle = new Dog("Snoopy");
+
+console.log(beagle.numLegs);

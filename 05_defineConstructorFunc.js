41466e1223e2d4d3b982c87aadb6f725e948402f
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 15:07:46 +0300

    Write a constructor function

--- 05_defineConstructorFunc.js
+++ 05_defineConstructorFunc.js
@@ -1,1 +1,13 @@
 // Define a Constructor Function
+
+// New objects are created with Constructor Functions
+
+function Bird() {
+    this.name = "Albert";
+    this.color = "blue";
+    this.numLegs = 2;
+}
+
+/* Constructors have capitalized names, use the keyword this
+to set properties of the object they create, and define 
+properties instead of returning values. */

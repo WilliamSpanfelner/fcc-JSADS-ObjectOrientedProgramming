6bd66a9e4aa73ae1a7c3db6ac03f8072bdc7ddef
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 14:56:14 +0300

    Access object properties using this keyword

--- 04_thisKeyword.js
+++ 04_thisKeyword.js
@@ -1,1 +1,11 @@
 // Make Code More Reusable with the this Keyword
+
+// Access property values using the this keyword
+
+let duck = {
+    name: "Aflac",
+    numLegs: 2,
+    sayName: function() {return "The name of this duck is " + this.name + ".";}
+};
+
+console.log(duck.sayName());

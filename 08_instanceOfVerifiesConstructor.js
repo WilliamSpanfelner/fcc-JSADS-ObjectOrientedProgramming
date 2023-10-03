dfb5c3f7f208d2e5c9aded036b910f5b7c3ffd96
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-03 17:08:30 +0300

    Use instanceof to see if an instance compares to constructor

--- 08_instanceOfVerifiesConstructor.js
+++ 08_instanceOfVerifiesConstructor.js
@@ -1,1 +1,14 @@
 // Verify an Object's Constructor with instanceof
+
+/*instanceof returns a boolean indicating whether or not
+an instance was created with a constructor. */
+
+let Bird = function(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 2;
+}
+
+let crow = new Bird("Alexis", "black");
+
+console.log(crow instanceof Bird);  // true

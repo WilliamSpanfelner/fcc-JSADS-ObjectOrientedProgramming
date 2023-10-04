e1a1cde75cc248b76b9434a1830c0977ee236053
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 10:59:45 +0300

    Set prototype constructor value

--- 14_setConstructorPropery.js
+++ 14_setConstructorPropery.js
@@ -1,2 +1,26 @@
 /* Remember to Set the Constructor Property when Changing 
 the Prototype */
+function Bird(name) {
+    this.name = name;
+}
+
+function Cartoon(name) {
+    this.name = name;
+}
+
+Bird.prototype = {
+    constructor: Bird,
+    numLegs: 2,
+    eat: function() {
+        console.log("nom nom nom");
+    },
+    describe: function() {
+        console.log("My name is " + this.name);
+    }
+};
+
+let duck = Bird("Daffy");
+duck.constructor = Cartoon;  // TypeError: Cannot read properties of undefined
+
+// console.log(duck.constructor === Bird);
+// console.log(duck.constructor === Cartoon);

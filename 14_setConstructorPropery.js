84c9da4bca28b12fdeebb509c4d3592f66888c60
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 11:01:39 +0300

    Challenge - start

--- 14_setConstructorPropery.js
+++ 14_setConstructorPropery.js
@@ -20,7 +20,24 @@ Bird.prototype = {
 };
 
 let duck = Bird("Daffy");
-duck.constructor = Cartoon;  // TypeError: Cannot read properties of undefined
+// duck.constructor = Cartoon;  // TypeError: Cannot read properties of undefined
 
 // console.log(duck.constructor === Bird);
 // console.log(duck.constructor === Cartoon);
+
+// Define the constructor property on the Dog prototype.
+function Dog(name) {
+    this.name = name;
+  }
+  
+  // Only change code below this line
+  Dog.prototype = {
+    
+    numLegs: 4,
+    eat: function() {
+      console.log("nom nom nom");
+    },
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
+  };

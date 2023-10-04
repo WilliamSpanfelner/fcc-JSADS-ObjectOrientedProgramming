30b42df62600e3f740b10ccae4271dffa08ad0d3
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 15:04:44 +0300

    Set object's prototype's constructor property manually

--- 20_resetInheritedConstructorProperty.js
+++ 20_resetInheritedConstructorProperty.js
@@ -1,1 +1,10 @@
 // Reset an Inherited Constructor Property
+function Animal() {}
+function Bird() {}
+Bird.prototype = Object.create(Animal.prototype);
+let duck = new Bird();
+console.log(duck.constructor);  // ƒ Animal() {}
+/* duck is an instance of Bird meaning that to show its
+value must be set manually */
+Bird.prototype.constructor = Bird;
+console.log(duck.constructor);  // ƒ Bird() {}

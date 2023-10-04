a846ebbc368002620e312a683aa53c8ef2f8a6ce
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 11:11:42 +0300

    Show relationship between instance & constructor

--- 15_originObjectPrototype.js
+++ 15_originObjectPrototype.js
@@ -1,1 +1,14 @@
 // Understand Where an Object’s Prototype Comes From
+
+/* An object inherits a prototype directly from the 
+constructor that created it. */
+
+function Bird(name) {
+    this.name = name;
+}
+
+let duck = new Bird("Daffy");
+
+/* Show the relationship between duck and Bird constructor
+as follows: */
+console.log(Bird.prototype.isPrototypeOf(duck));  // true

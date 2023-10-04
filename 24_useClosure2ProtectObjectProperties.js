9960c746637376888030951de0788c032c3e2452
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 18:08:57 +0300

    Challenge - start

--- 24_useClosure2ProtectObjectProperties.js
+++ 24_useClosure2ProtectObjectProperties.js
@@ -15,3 +15,12 @@ function Bird() {
 let ducky = new Bird();
 ducky.hatchedEgg = 20;  // attempting to set hatchedEgg to a new value has no effect.
 console.log(ducky.getHatchedEggCount());  // 10
+
+/* Change how weight is declared in the Bird function so 
+it is a private variable. Then, create a method getWeight 
+that returns the value of weight 15. */
+function Bird() {
+    this.weight = 15;
+  
+  
+}

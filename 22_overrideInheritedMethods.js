80fa73d5735da46989106799d74823730433d61d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 15:58:17 +0300

    Challenge - solution

--- 22_overrideInheritedMethods.js
+++ 22_overrideInheritedMethods.js
@@ -27,9 +27,11 @@ Penguin.prototype.constructor = Penguin;
 
 // Only change code below this line
 
-
+Penguin.prototype.fly = function() {
+    return "Alas, this is a flightless bird."
+};
 
 // Only change code above this line
 
 let penguin = new Penguin();
-console.log(penguin.fly());
+console.log(penguin.fly());  // Alas, this is a flightless bird.

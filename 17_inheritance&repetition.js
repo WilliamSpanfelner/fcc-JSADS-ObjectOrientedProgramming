77ef21a746e792b284f71266853da9eba8ac0b28
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 12:18:26 +0300

    Create a supertype to eliminate method repetition

--- 17_inheritance&repetition.js
+++ 17_inheritance&repetition.js
@@ -1,1 +1,45 @@
 // Use Inheritance So You Don't Repeat Yourself
+// Don't Repeat Yourself (DRY)
+/* Consider how the following objects share the same 
+for the describe property value. */
+function Bird(name) {
+    this.name = name;
+}
+
+function Dog(name) {
+    this.name = name;
+}
+
+Bird.prototype = {
+    constructor: Bird,
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
+};
+  
+Dog.prototype = {
+    constructor: Dog,
+    describe: function() {
+        console.log("My name is " + this.name);
+    }
+};
+  
+/* The repitition of the describe method can be eliminated
+through the definition of a supertype */
+
+function Animal() {};
+
+Animal.prototype = {
+    describe: function() {
+        console.log("My name is " + this.name);
+    }
+};
+
+// Now the describe method can be removed from the prototypes
+Bird.prototype = {
+    constructor: Bird
+};
+  
+Dog.prototype = {
+    constructor: Dog
+};

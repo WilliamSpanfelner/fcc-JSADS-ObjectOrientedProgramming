33b83d8e769c901907cc025d9f3aa4cda64e7ba1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 17:16:37 +0300

    Use mixin to give behaviour to unrelated objects

--- 23_useMixin.js
+++ 23_useMixin.js
@@ -1,2 +1,27 @@
 /* Use a Mixin to Add Common Behavior Between 
 Unrelated Objects  */
+/* A mixin allows other objects to use a collection 
+of functions. */
+let flyMixin = function(obj) {
+    obj.fly = function() {
+        console.log("Flying, wooosh!");
+    }
+};
+
+let bird = {
+    name: "Fred",
+    numLegs: 2
+};
+
+let plane = {
+    model: "777",
+    numPassengers: 524
+};
+
+/* Passing bird and plane into the flying mixin will mean
+they will both have the ability to fly. */
+flyMixin(bird);
+flyMixin(plane);
+
+bird.fly();  // Flying, wooosh!
+plane.fly();  // Flying, wooosh!

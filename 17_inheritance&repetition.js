ec6b14b657ec6438221c9e35432dbfa0f1472c53
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 12:23:47 +0300

    Challenge - solution

--- 17_inheritance&repetition.js
+++ 17_inheritance&repetition.js
@@ -53,9 +53,6 @@ function Cat(name) {
   
 Cat.prototype = {
     constructor: Cat,
-    eat: function() {
-      console.log("nom nom nom");
-    }
 };
   
 function Bear(name) {
@@ -63,15 +60,14 @@ function Bear(name) {
 }
   
 Bear.prototype = {
-    constructor: Bear,
-    eat: function() {
-      console.log("nom nom nom");
-    }
+    constructor: Bear
 };
   
 function Animal() { }
   
 Animal.prototype = {
     constructor: Animal,
-  
+    eat: function() {
+        console.log("nom nom nom");
+    }
 };

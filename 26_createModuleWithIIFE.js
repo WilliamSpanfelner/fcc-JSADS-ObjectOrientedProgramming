b247f94b19492a2b0d8b69a7787f2d9a67204917
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 18:43:31 +0300

    Create mixins module using IIFE

--- 26_createModuleWithIIFE.js
+++ 26_createModuleWithIIFE.js
@@ -1,1 +1,29 @@
 // Use an IIFE to Create a Module
+/* Immediately Invoked Function Expressions can be used
+to package a module like a set of mixins */
+let motionModule = (function () {
+    return {
+        glideMixin: function(obj) {
+            obj.glide = function() {
+                console.log("Gliding on the water");
+            };
+        },
+        flyMixin: function(obj) {
+            obj.fly = function() {
+                console.log("Flying, wooosh!");
+            };
+        }
+    }
+})();
+
+function Bird() {
+    let hatchedEgg = 10;
+    this.getHatchedEggCount = function() {
+        return hatchedEgg;
+    };
+}
+
+let ducky = new Bird();
+
+motionModule.glideMixin(ducky);
+ducky.glide()  // Gliding on the water

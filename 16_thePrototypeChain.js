937bd1b0e043ea64e173f78e1d9e0fcb62a13d64
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 11:41:24 +0300

    Challenge - solution

--- 16_thePrototypeChain.js
+++ 16_thePrototypeChain.js
@@ -24,11 +24,11 @@ console.log(duck.hasOwnProperty("name"));  // true
 // Modify the code to show the correct prototype chain.
 function Dog(name) {
     this.name = name;
-  }
-  
-  let beagle = new Dog("Snoopy");
-  
-  Dog.prototype.isPrototypeOf(beagle);  // yields true
-  
-  // Fix the code below so that it evaluates to true
-  ???.isPrototypeOf(Dog.prototype);
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+console.log(Object.prototype.isPrototypeOf(Dog.prototype));  // true

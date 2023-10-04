9247b22ccbc960955eea1f64e75c76f68a2ac34b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 18:29:58 +0300

    Challenge - start

--- 25_iife.js
+++ 25_iife.js
@@ -9,3 +9,12 @@ as soon as it is declared: */
 
 /* This pattern is known as an immediately invoked 
 function expression or IIFE  */
+
+/* Rewrite the function makeNest and remove its call so 
+instead it's an anonymous immediately invoked function 
+expression (IIFE).  */
+function makeNest() {
+    console.log("A cozy nest is ready");
+}
+
+makeNest();

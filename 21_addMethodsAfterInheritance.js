dfacc8a34bcd49596a06f3c9a708a8fbf81a9e03
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 15:27:33 +0300

    Create method unique to child or parent objects

--- 21_addMethodsAfterInheritance.js
+++ 21_addMethodsAfterInheritance.js
@@ -1,1 +1,21 @@
 // Add Methods After Inheritance
+/* A constructor function that inherits its prototype object from a 
+supertype constructor function can still have its own methods in
+addition to inherited methods. */
+
+function Animal() {}
+Animal.prototype.eat = function() {
+    console.log("nom nom nom");
+};
+function Bird() {}
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.constructor = Bird;
+
+// To add a behaviour unique to Bird objects:
+Bird.prototype.fly = function() {
+    console.log("I'm flying!");
+};
+
+let duck = new Bird();
+duck.eat();  // nom nom nom
+duck.fly();  // I'm flying!

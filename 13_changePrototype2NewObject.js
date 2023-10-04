e17d0b98f052d3248db69d630f46c979a3a9bf77
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 09:45:17 +0300

    Create Bird prototype object

--- 13_changePrototype2NewObject.js
+++ 13_changePrototype2NewObject.js
@@ -1,1 +1,22 @@
 // Change the Prototype to a New Object
+/* Adding prototype properties individually is tedious.  
+Avoid the tedium by creating a prototype object */
+
+function Bird(name) {
+    this.name = name;
+}
+
+Bird.prototype = {
+    numLegs: 2,
+    eat: function() {
+        console.log("nom nom nom");
+    },
+    describe: function() {
+        console.log("My name is " + this.name);
+    }
+}
+
+let roadrunner = new Bird("Road Runner");
+console.log(roadrunner.numLegs);  // 2
+roadrunner.eat();  // nom nom nom
+roadrunner.describe();  // My name is Road Runner

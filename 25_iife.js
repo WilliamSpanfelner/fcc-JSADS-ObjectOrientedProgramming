d02991094cb86baf25a791bdd37ffc3e446ecdd8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 18:28:30 +0300

    Write and IIFE

--- 25_iife.js
+++ 25_iife.js
@@ -1,2 +1,11 @@
 /* Understand the Immediately Invoked Function 
 Expression (IIFE) */
+
+/* A common pattern in JavaScript is to execute a function
+as soon as it is declared: */
+(function() {
+    console.log("Chirp, chirp!");
+})();  // Chirp, chirp!
+
+/* This pattern is known as an immediately invoked 
+function expression or IIFE  */

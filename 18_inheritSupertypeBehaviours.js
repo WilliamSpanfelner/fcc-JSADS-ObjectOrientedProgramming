84c69b7be67b39038bc704b5d1d4ad1f6f7a5018
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 14:28:48 +0300

    Instantiate with Object.create to set prototype

--- 18_inheritSupertypeBehaviours.js
+++ 18_inheritSupertypeBehaviours.js
@@ -5,3 +5,15 @@ function Animal() {};
 Animal.prototype.eat = function() {
     console.log("nom nom nom");
 }
+
+// Create an instance of the supertype
+// let animal = new Animal();
+
+// or
+
+let animal = Object.create(Animal.prototype);
+/* Object.create(obj) creates a new object and sets obj as
+the new object's prototype */
+
+animal.eat();  // nom nom nom
+console.log(animal instanceof Animal);  // true

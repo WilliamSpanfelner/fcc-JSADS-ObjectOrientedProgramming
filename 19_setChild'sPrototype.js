d049570bb3fcd8909a60e0527c305b9a0f698967
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 14:49:19 +0300

    Set child's prototype to that of parent and observe inheritance

--- 19_setChild'sPrototype.js
+++ 19_setChild'sPrototype.js
@@ -1,1 +1,15 @@
 // Set the Child's Prototype to an Instance of the Parent
+
+function Animal() {};
+Animal.prototype.eat = function() {
+    console.log("nom nom nom");
+}
+
+function Bird(name) {
+    this.name = name;
+}
+
+Bird.prototype = Object.create(Animal.prototype);
+
+let duck = new Bird("Daffy");
+duck.eat();  // nom nom nom

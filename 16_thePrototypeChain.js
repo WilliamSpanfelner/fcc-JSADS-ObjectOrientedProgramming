3a327f177a4983b6dc3b295eedf80b050589e7fc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 11:37:14 +0300

    Illustrate prototype chain

--- 16_thePrototypeChain.js
+++ 16_thePrototypeChain.js
@@ -7,3 +7,16 @@ function Bird(name) {
 }
 
 console.log(typeof Bird.prototype);  // object
+
+console.log(Object.prototype.isPrototypeOf(Bird.prototype));  // true
+
+// Let's illustrate the prototype chain:
+let duck = new Bird("Daffy");
+console.log(duck.hasOwnProperty("name"));  // true
+
+// hasOwnProperty is defined in Object.prototype;
+// Bird.prototype has access to Object.prototype;
+// duck has access to Bird.prototype;
+// Bird is the supertype for the subtype duck;
+// Object is the supertype for Bird and duck;
+// Object is a supertype of all object in JavaScript; 

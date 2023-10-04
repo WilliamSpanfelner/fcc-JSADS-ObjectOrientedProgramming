8964bb18d2f20bfa9a822e7bd66f515d76df0891
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 19:09:34 +0300

    Update 04_thisKeyword.js

--- 04_thisKeyword.js
+++ 04_thisKeyword.js
@@ -20,6 +20,6 @@ let dog = {
     name: "Spot",
     numLegs: 4,
     sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
-  };
-  
-  console.log(dog.sayLegs());
+};
+
+console.log(dog.sayLegs());

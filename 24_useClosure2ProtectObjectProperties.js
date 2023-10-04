54ef7a0e0d0b0fedec94e2139bd55c05cc83516c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 18:06:24 +0300

    Create a private variable in a constructor

--- 24_useClosure2ProtectObjectProperties.js
+++ 24_useClosure2ProtectObjectProperties.js
@@ -1,3 +1,17 @@
 /* Use Closure to Protect Properties Within an 
 Object from Being Modified Externally */
 
+/* A function always has access to the context
+in which it was created.  This is called closure in 
+JavaScript */
+
+function Bird() {
+    let hatchedEgg = 10;
+    this.getHatchedEggCount = function() {
+        return hatchedEgg;
+    };
+}
+
+let ducky = new Bird();
+ducky.hatchedEgg = 20;  // attempting to set hatchedEgg to a new value has no effect.
+console.log(ducky.getHatchedEggCount());  // 10

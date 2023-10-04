62fef9a36e2cea61043af405bedf7d77a50a54d1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 09:17:38 +0300

    Use the constructor property

--- 12_constructorProperty.js
+++ 12_constructorProperty.js
@@ -1,1 +1,19 @@
 // Understand the Constructor Property
+/* The constructor property is a reference to the constructor
+function that created the instance.  The constructor 
+property can determine what kind of object it is. */
+
+function Bird(name) {
+    this.name = name;
+}
+
+function Dog(name) {
+    this.name = name;
+}
+
+let duck = new Bird();
+let beagle = new Dog();
+
+console.log(duck.constructor === Bird);  // true
+console.log(beagle.constructor === Dog);  // true
+

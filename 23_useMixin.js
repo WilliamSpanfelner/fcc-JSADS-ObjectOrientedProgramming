f2ffd78cc16dd5c9d39e26ca96c43e9ee01e20cf
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 17:22:45 +0300

    Challenge - start

--- 23_useMixin.js
+++ 23_useMixin.js
@@ -25,3 +25,13 @@ flyMixin(plane);
 
 bird.fly();  // Flying, wooosh!
 plane.fly();  // Flying, wooosh!
+
+/* Create a mixin named glideMixin that defines a method 
+named glide. Then use the glideMixin to give both bird 
+and boat the ability to glide. */
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+};
+  
+// Only change code below this line

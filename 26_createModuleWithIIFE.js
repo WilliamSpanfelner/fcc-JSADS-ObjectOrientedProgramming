a66aea7c3a3814d265445ad963160df64f5feb37
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 18:54:15 +0300

    Challenge - solution

--- 26_createModuleWithIIFE.js
+++ 26_createModuleWithIIFE.js
@@ -31,13 +31,17 @@ ducky.glide()  // Gliding on the water
 /* Create a module named funModule to wrap the two mixins 
 isCuteMixin and singMixin. funModule should return 
 an object. */
-let isCuteMixin = function(obj) {
-    obj.isCute = function() {
-      return true;
-    };
-};
-let singMixin = function(obj) {
-    obj.sing = function() {
-      console.log("Singing to an awesome tune");
-    };
-};
+let funModule = (function () {
+    return {
+        isCuteMixin: function(obj) {
+            obj.isCute = function() {
+            return true;
+            };
+        },
+        singMixin: function(obj) {
+            obj.sing = function() {
+            console.log("Singing to an awesome tune");
+            };
+        }
+    }
+})();

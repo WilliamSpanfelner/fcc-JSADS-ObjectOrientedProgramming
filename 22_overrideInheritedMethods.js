4668d225e11b8a128543be1c213424f03a51f929
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 15:50:44 +0300

    Override parent method

--- 22_overrideInheritedMethods.js
+++ 22_overrideInheritedMethods.js
@@ -1,1 +1,16 @@
 // Override Inherited Methods
+/* Overriding an inherited method is done by using the 
+same method name on the child object as in the parent. */
+function Animal() {}
+Animal.prototype.eat = function() {
+    return "nom nom nom";
+};
+function Bird() {}
+
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.eat = function() {
+    return "peck peck peck";
+}
+
+let duck = new Bird();
+console.log(duck.eat());  // peck peck peck

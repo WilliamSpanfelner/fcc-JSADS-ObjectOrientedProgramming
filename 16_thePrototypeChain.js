cae4fb916c9df3976739492e4d47fb5094198185
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 11:21:11 +0300

    Use typeof to show the type of an object's prototype

--- 16_thePrototypeChain.js
+++ 16_thePrototypeChain.js
@@ -1,1 +1,9 @@
 // Understand the Prototype Chain
+
+/* All objects have a prototype and an object's prototype 
+itself is an object. */
+function Bird(name) {
+    this.name = name;
+}
+
+console.log(typeof Bird.prototype);  // object

d4444870b261b54bcd9dee15d332d4ff1b990ed8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 18:32:39 +0300

    Challenge - solution

--- 25_iife.js
+++ 25_iife.js
@@ -13,8 +13,6 @@ function expression or IIFE  */
 /* Rewrite the function makeNest and remove its call so 
 instead it's an anonymous immediately invoked function 
 expression (IIFE).  */
-function makeNest() {
+(function() {
     console.log("A cozy nest is ready");
-}
-
-makeNest();
+})();  // A cozy nest is ready

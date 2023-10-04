bd19842f25e36f63ae074b74fdfb8b49304f3665
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-04 15:54:00 +0300

    Challenge - start

--- 22_overrideInheritedMethods.js
+++ 22_overrideInheritedMethods.js
@@ -14,3 +14,22 @@ Bird.prototype.eat = function() {
 
 let duck = new Bird();
 console.log(duck.eat());  // peck peck peck
+
+/* Override the fly() method for Penguin so that it 
+returns the string Alas, this is a flightless bird. */
+// function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
